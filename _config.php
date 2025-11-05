85b60ded6db6f6b2cbc000a7abf5c94747d9f63b
umeshrayabbuchak-gif <umeshrayabbuchak@gmail.com>
2025-11-05 08:58:04 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -3,4 +3,6 @@ $yourName = ""; // Your real first name
 $yourGfName = ""; // Your real GF first name
 $yourFullName = ""; // Real full name
 $yourEmail = ""; // your email
+$yourNameFirstLetter = ""; // your name first letter
+$yourGfNameFirstLetter = ""; // your gf name first letter
 ?>
